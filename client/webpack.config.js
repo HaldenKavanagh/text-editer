3be328391eac7d09c000fce47c27b7a68207bab6
Halden Kavanagh <haldenkav@gmail.com>
2024-02-06 21:52:43 -0700

    removed app from develope folder

---
+++ client/webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: "development",
+    entry: {
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+      }),
+      new WebpackPwaManifest({
+        name: "Jate",
+        short_name: "jate",
+        description: "just another text editor",
+        background_color: "#1e1e1e",
+        theme_color: "#1e1e1e",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
